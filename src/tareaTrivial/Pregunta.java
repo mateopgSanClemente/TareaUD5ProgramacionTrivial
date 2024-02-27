59882b0b1c8b8f17fddbdee949895d57863de9b9
MateoPG <A21MateoPG@iessanclemente.es>
2024-02-27 16:50:42 +0100

    Inicio do proxecto. Crearonse al clases

---
+++ src/tareaTrivial/Pregunta.java
@@ -0,0 +1,5 @@
+package tareaTrivial;
+
+public class Pregunta {
+
+}
---
+++ src/tareaTrivial/Xogar.java
@@ -0,0 +1,10 @@
+package tareaTrivial;
+
+public class Xogar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
